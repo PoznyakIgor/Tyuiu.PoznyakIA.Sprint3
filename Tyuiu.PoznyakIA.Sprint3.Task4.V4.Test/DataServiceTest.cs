8c5be5b8f3377cdc13f8d9cd0b325e883b472425
Tyuiu.PoznyakIA@outlook.com <igor.poznyak.2005@mail.ru>
2023-10-29 23:33:56 +0500

    Task4

---
+++ Tyuiu.PoznyakIA.Sprint3.Task4.V4.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PoznyakIA.Sprint3.Task4.V4.Lib;
+namespace Tyuiu.PoznyakIA.Sprint3.Task4.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            Assert.AreEqual(-35.317, ds.Calculate(-5, 5));
+        }
+    }
+}
---
+++ Tyuiu.PoznyakIA.Sprint3.Task4.V4.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PoznyakIA.Sprint3.Task4.V4.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PoznyakIA.Sprint3.Task4.V4.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b621a8a5-a28c-4517-ad85-40ea993fb5e2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
