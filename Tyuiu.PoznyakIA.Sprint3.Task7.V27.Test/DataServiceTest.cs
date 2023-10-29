71081a08c950a30a72918ca5be15a4c0cf8a9de4
Tyuiu.PoznyakIA@outlook.com <igor.poznyak.2005@mail.ru>
2023-10-30 00:33:12 +0500

    Task7

---
+++ Tyuiu.PoznyakIA.Sprint3.Task7.V27.Test/DataServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PoznyakIA.Sprint3.Task7.V27.Lib;
+
+namespace Tyuiu.PoznyakIA.Sprint3.Task7.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            int len = 1 + (stopValue - startValue);
+
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+            valueWaitArray[0] = -13.25;
+            valueWaitArray[1] = -10.93;
+            valueWaitArray[2] = -7.22;
+            valueWaitArray[3] = -5.04;
+            valueWaitArray[4] = 0;
+            valueWaitArray[5] = -0.30;
+            valueWaitArray[6] = 2.57;
+            valueWaitArray[7] = 6.40;
+            valueWaitArray[8] = 10.04;
+            valueWaitArray[9] = 12.72;
+            valueWaitArray[10] = 14.68;
+
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(valueWaitArray, res);
+        }
+    }
+}
---
+++ Tyuiu.PoznyakIA.Sprint3.Task7.V27/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PoznyakIA.Sprint3.Task7.V27.Lib;
+
+namespace Tyuiu.PoznyakIA.Sprint3.Task7.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Позняк И.А | ИСТНб-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнил: Позняк И.А | ИСТНб-23-1                                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу значений функции            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int endValue = 5;
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + endValue);
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double[] array = ds.GetMassFunction(startValue, endValue);
+
+            Console.WriteLine("+------------+------------+");
+            Console.WriteLine("|      X     |    f(x)    |");
+            Console.WriteLine("+------------+------------+");
+
+            foreach (double i in array)
+            {
+                Console.WriteLine("|{0,7:d}     |  {1, 7:f2}   |", startValue++, i);
+            }
+
+            Console.WriteLine("+------------+------------+");
+            Console.ReadKey();
+        }
+    }
+}
